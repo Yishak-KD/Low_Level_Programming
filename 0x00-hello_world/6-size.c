b80400f5be334732fa7dbab88d12340683f3482e
Yishak Kidanu <yisak04@gmail.com>
2021-06-17 13:07:41 -0700

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ float floatType;
 printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(charType));
 printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(intType));
 printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(llType));
-printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(lIntType));
+printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(llType));
 printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(floatType));
 return (0);
 }
