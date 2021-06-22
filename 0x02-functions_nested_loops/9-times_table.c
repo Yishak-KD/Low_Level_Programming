a47ca07950efe970f7420166a5d98bf04f2d2cb5
Yishak Kidanu <yisak04@gmail.com>
2021-06-22 12:00:41 -0700

    a function that prints the 9 times table, starting with 0.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,30 +1,36 @@
-#include <stdio.h>
 #include "holberton.h"
 
 /**
- * print_to_98 - prints natural numbers from n to 98 followed by a new line
- * @n: print from the listed natural numbers
+ * times_table - prints the 9 times table, beginning from 0.
  */
-void print_to_98(int n)
+void times_table(void)
 {
-	int i, j;
-	if (n <= 98)
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (i = n; i <= 98; i++)
+		for (j = 0; j < 10; j++)
 		{
-			if (i != 98)
-				printf("%d, ", i);
-			else if (i == 98)
-				printf("%d\n", i);
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
-	} else if (n >= 98)
-	{
-		for (j = n; j >= 98; j--)
-		{
-			if (j != 98)
-				printf("%d, ", j);
-			else if (j == 98)
-				printf("%d\n", j);
-		}
+		_putchar('\n');
 	}
 }
