c797fd10b63d1954a2427f70f7e6d0c4251a0c09
Yishak Kidanu <yisak04@gmail.com>
2021-06-24 09:07:11 -0700

    More functions and nested loops.

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,23 +1,17 @@
 #include "holberton.h"
 
 /**
- * print_most_numbers - prints numbers
- * Return:0
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
  */
-
-void print_most_numbers(void)
+void print_numbers(void)
 {
-	char n = 0;
+	int i;
 
-	while (n <= 9)
+	for (i = 0; i < 10; i++)
 	{
-		if (n != 2 && n != 4)
-		{
-			_putchar('0' + n);
-		}
-
-		n++;
+		_putchar(i + '0');
 	}
-	_putchar('\n');
 
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,21 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
-* more_numbers - numbers to consider.
-*/
-
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
+ */
 void more_numbers(void)
 {
-int i = 0, j;
-while (i < 10)
-{
-j = 0;
-while (j < 15)
-{
-_putchar("%d", j);
-j++;
-}
-_putchar("\n");
-i++;
-}
-}
+	int i, j;
 
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
