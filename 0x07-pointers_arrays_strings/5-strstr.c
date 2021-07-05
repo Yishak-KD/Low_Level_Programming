0a692ef57c5a86244c9f6df6ba55ca5c187dd2b9
Yishak Kidanu <yisak04@gmail.com>
2021-07-05 09:30:00 -0700

    More on pointers, arrays and strings.

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _strstr-locates a substring
+ * @haystack:string to check occurence of needle
+ * @needle:string to be checked.
+ * Return:Returns a pointer to the beginning of the located
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *i = haystack;
+		char *j = needle;
+
+		while (*haystack && *j && *haystack == *j)
+		{
+			haystack++;
+			j++;
+		}
+
+		if (!*j)
+			return (i);
+
+		haystack = i + 1;
+	}
+	return ('\0');
+}
